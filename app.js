eb6927994e32689d885ed9b9bbed2e8cbf7be569
tt142857 <timelimit12@naver.com>
2023-04-19 10:05:44 +0900

    commit

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const path = require('path');
 const app = express();
 const server = http.createServer(app);
 
-const hostname = '172.31.45.216';
-const port = 3003;
+const hostname = '172.31.41.85';
+const port = 8080;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -22,6 +22,7 @@ app.get('/test', (req, res) => {
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`connect on http://ec2-15-164-129-37.ap-northeast-2.compute.amazonaws.com:${port}`);
 });
 
 module.exports = app;
